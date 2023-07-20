c00aca32f457079fdefb0a37886ada729fbb69f8
theMogget <y.kaiyi@outlook.com>
2023-07-20 14:10:03 +0800

    feat: make scripts delete geo rows

--- traffic_flags/traffic_flags_sea.py
+++ traffic_flags/traffic_flags_sea.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import datetime
 from context import cnx
+from sqlalchemy.orm import sessionmaker
+
 
 traffic_query = '''
 select 
@@ -14,8 +16,26 @@ select
     and cl.spc_geo = 'SEA'
 '''
 
+delete_traffic_query = '''
+    DO
+    $$
+    BEGIN
+        IF EXISTS (
+            SELECT FROM information_schema.tables 
+            WHERE  table_schema = 'score_v2'
+            AND    table_name   = 'traffic_flags'
+        )
+        THEN 
+            DELETE FROM score_v2.traffic_flags WHERE spc_geo = 'SEA';
+        END IF;
+    END
+    $$
+'''
+
 if __name__ == '__main__':
+    print('[{}] Starting traffic_flags_sea.py'.format(datetime.now()))
     conn = cnx.Cnx
+    session = sessionmaker(bind=conn)()
 
     # pull data
     print('[{}] Pulling traffic...'.format(datetime.now()))
@@ -33,13 +53,23 @@ if __name__ == '__main__':
     traffic['ranks'] = traffic_grouped['r_squared'].rank(method='min', ascending=False)
 
     # assign flag to top 25 of each
-    traffic['is_traffic_priority'] = traffic['ranks'] <= 25
+    traffic['is_traffic_priority'] = traffic['ranks'] <= 50
 
     # write to db
+    print('[{}] Deleting old SEA data'.format(datetime.now()))
+    try:
+        session.execute(delete_traffic_query)
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
     print('[{}] Writing to db'.format(datetime.now()))
     to_write = traffic[['company_id', 'is_traffic_priority']]
     to_write['generated_at'] = datetime.now()
+    to_write['spc_geo'] = 'SEA'
     to_write.to_sql(
-        'traffic_flags', conn, if_exists='replace', index=False, schema='score_v2'
+        'traffic_flags', conn, if_exists='append', index=False, schema='score_v2'
     )
     print('[{}] Done writing to db'.format(datetime.now()))
